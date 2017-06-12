14671b9f66eafd0924bb87817517ebc4330d0ba9
jangdongho <jaebul2006@naver.com>
2017-06-12 23:28:17 +0900

    camera resolution set

--- Assets/Scripts/CamMgr.cs
+++ Assets/Scripts/CamMgr.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 // 화면은 항상 중앙, 좌우가 준비된 화면이어야 한다.
 public class CamMgr : MonoBehaviour 
 {
+	float TEN_SIXTEEN_RATIO = 0.625f;
 
 	void Start () 
     {
-		
+		ScrResolution ();	
 	}
 	
 	void Update () 
     {
 		
+	}
+
+	private void ScrResolution()
+	{
+		// 목표 해상도는 16:10.
+//		float cur_ratio = (float)Screen.width / (float)Screen.height;
+//		if (cur_ratio < TEN_SIXTEEN_RATIO) {
+//			Camera.main.aspect = 1f/1.6f;
+//		}
+		//Camera.main.aspect = 1f/1.6f;
+		Camera.main.aspect = TEN_SIXTEEN_RATIO;
 	}
 
 }
