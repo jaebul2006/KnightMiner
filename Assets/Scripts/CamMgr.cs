e17841f765e97209c084053e285369c72dbacd20
jdh <jaebul2006@naver.com>
2017-06-12 18:00:44 +0900

    kg project start

---
+++ Assets/Scripts/CamMgr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 화면은 항상 중앙, 좌우가 준비된 화면이어야 한다.
+public class CamMgr : MonoBehaviour 
+{
+
+	void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+}
